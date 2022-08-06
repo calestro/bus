994b6316f0271304a6b806380dada6b7594ef54a
Leonir Júnior Ribeiro Calestro <105824089+calestro@users.noreply.github.com>
2022-08-06 00:03:47 -0300

    criando arquivos

---
+++ lib/Activy/mainactivy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MainActivy extends StatefulWidget {
+  const MainActivy({Key? key}) : super(key: key);
+
+  @override
+  State<MainActivy> createState() => _MainActivyState();
+}
+
+class _MainActivyState extends State<MainActivy> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import 'Activy/mainactivy.dart';
+
+
+
+void main (){
+  runApp(MaterialApp(
+    home: MainActivy(),
+  ));
+}
